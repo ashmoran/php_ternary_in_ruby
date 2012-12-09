41e2b7acb318a4ca4f57c28557d23fb65f5716cd
Ash Moran <ash.moran@patchspace.co.uk>
2012-12-09 22:48:46 +0000

    Hideously ugly hack to implement PHP semantics: needs both "ternary" and "nested_ternary" grammar rules

--- spec/php_spec.rb
+++ spec/php_spec.rb
@@ -5,23 +5,58 @@ require 'php'
 describe PHP do
   let(:parser) { PHPParser.new }
 
+  # These are only here to unit-test the language components
+  describe "expressions" do
+    describe "booleans" do
+      specify {
+        expect(parser.parse("TRUE").eval).to be_true
+      }
+
+      specify {
+        expect(parser.parse("FALSE").eval).to be_false
+      }
+    end
+
+    describe "strings" do
+      example do
+        expect(parser.parse('"a"').eval).to be == "a"
+      end
+    end
+  end
+
+  # This is the point of the exercise
   describe "ternary operator" do
     context "simple" do
-      example do
-        expect(
-          parser.parse('TRUE ? "a" : "b"').eval
-        ).to be == "a"
+      context "boolean condition" do
+        example do
+          expect(
+            parser.parse('TRUE ? "a" : "b"').eval
+          ).to be == "a"
+        end
+
+        example do
+          expect(
+            parser.parse('FALSE ? "a" : "b"').eval
+          ).to be == "b"
+        end
       end
 
-      example do
-        expect(
-          parser.parse('FALSE ? "a" : "b"').eval
-        ).to be == "b"
+      context "string condition" do
+        example do
+          expect(
+            parser.parse('"x" ? "a" : "b"').eval
+          ).to be == "a"
+        end
       end
     end
 
+    # And this is the nut I'm trying to crack
     context "nested" do
       example do
+        parser.parse('TRUE ? "a" : TRUE ? "b" : "c"')
+        puts
+        puts parser.failure_reason
+        puts
         expect(
           parser.parse('TRUE ? "a" : TRUE ? "b" : "c"').eval
         ).to be == "b"
